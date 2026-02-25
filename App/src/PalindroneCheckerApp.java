7e15f58ff3da89bdc8971b8ad8a9da5b15b9643e
Lakshana-hub <lb9943@srmist.edu.in>
2026-02-25 09:45:05 +0530

    Added System initialization Successfuly

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,5 +1,29 @@
+/**
+ * ==============================================================================================
+ * MAIN CLASS -UseCase1PalindroneApp
+ * ==============================================================================================
+ *
+ * USE Case 1: Application entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the Palindrone Checker Management System.
+ *
+ * At this stage,the application :
+ * -starts execution from the main () method
+ * -display a welcome message
+ * -show application version
+ *
+ * No PALINDROME LOGIC IS IMPLEMENTED YET.
+ *
+ * THE GOAL IS TO ESTABLISH A CLEAR STARTUP FLOW.
+ *
+ * @author Developer
+ * @version 1.0
+ */
 public class PalindroneCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Managment System.");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # PalindroneCheckerApp
 
-// running procedure
-//flow of project
-//objective
-//what are topics it will be covered
-//which usecase
